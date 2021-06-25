f1f49a5c181023304fb90c98a67b2153710d319b
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-06-25 17:25:55 +0900

    first commit

---
+++ PictureViewer/PictureViewer/Database.hpp
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <Siv3D.hpp>
+#include "sqlite3.h"
+
+namespace db
+{
+	sqlite3* connection = nullptr;
+	String path = U"./database.db";
+
+	const int sizeofString(const std::string& str)
+	{
+		return str.cend() - str.cbegin();
+	}
+
+	const bool existsTable(const String& tablename)
+	{
+		const String exec = U"select count(*) from sqlite_master where type = 'table' AND name=?;";
+		sqlite3_stmt* statement;
+
+		const std::string tablename_utf8 = tablename.toUTF8();
+
+		sqlite3_prepare(connection, exec.toUTF8().c_str(), -1, &statement, NULL);
+		sqlite3_bind_text(statement, 1, tablename_utf8.c_str(), sizeofString(tablename_utf8), SQLITE_TRANSIENT);
+
+		int col_count = sqlite3_column_count(statement);
+		if (col_count == 1)
+		{
+			sqlite3_step(statement);
+			int exists = sqlite3_column_int(statement, 1);
+
+			if (exists == 1)
+				return true;
+		}
+		return false;
+	}
+
+	void initializeTables()
+	{
+		{
+			const String exec = U"";
+		}
+	}
+}
---
+++ PictureViewer/PictureViewer/Main.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <Siv3D.hpp> // OpenSiv3D v0.4.3
+#include "Database.hpp"
+
+void Main()
+{
+	// 背景を水色にする
+	Scene::SetBackground(Palette::Gray);
+
+	setlocale(LC_ALL, "");
+
+	// 大きさ 60 のフォントを用意
+	const Font font(60);
+
+	sqlite3_open(db::path.toUTF8().c_str(), &db::connection);
+
+	while (System::Update())
+	{
+		
+	}
+
+	sqlite3_close(db::connection);
+}
