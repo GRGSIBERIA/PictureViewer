ef5e854ea7dca242209504a12e89bcf071c0a33e
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-07-04 00:51:00 +0900

    ハッシュ値を取得する関数を作る

--- PictureViewer/PictureViewer/ImageReader.hpp
+++ PictureViewer/PictureViewer/ImageReader.hpp
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #include <Siv3D.hpp>
 #include <sqlite3.h>
+#include <Windows.h>
 
 namespace image
 {
@@ -33,5 +34,34 @@ namespace image
 		memcpy_s(dest, source.size_bytes(), (void*)source.data(), source.size_bytes());
 
 		return dest;
+	}
+
+	/// <summary>
+	/// 画像からハッシュ値を取得する
+	/// </summary>
+	/// <param name="source">元の画像</param>
+	/// <returns>SHA256のダイジェスト</returns>
+	const String get_digest(const Image& source)
+	{
+		HCRYPTPROV prov;
+		HCRYPTHASH hash;
+		PBYTE digest;
+		DWORD cryptSize = 32;
+		String retval = U"";
+
+		CryptAcquireContext(&prov, NULL, NULL, PROV_RSA_AES, CRYPT_VERIFYCONTEXT);
+		CryptCreateHash(prov, CALG_SHA_256, 0, 0, &hash);
+		CryptHashData(hash, (BYTE*)source.data(), (DWORD)source.size_bytes(), 0);
+
+		digest = (BYTE*)malloc(cryptSize);
+		CryptGetHashParam(hash, HP_HASHVAL, digest, &cryptSize, 0);
+
+		std::string buf;
+		buf.assign(digest, digest + cryptSize);
+		retval = Unicode::WidenAscii(buf);
+
+		free(digest);
+
+		return retval;
 	}
 }
