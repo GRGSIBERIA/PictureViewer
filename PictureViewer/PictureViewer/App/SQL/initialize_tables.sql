a78d206fee164d86265d93de0100fa45727ab5a8
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-07-06 23:46:42 +0900

    リソースとコードの分離ができていなくて気持ち悪いので、まとめて実行できるように修正中

---
+++ PictureViewer/PictureViewer/App/SQL/initialize_tables.sql
@@ -0,0 +1,26 @@
+create table if not exists image_t (
+	id integer primary key autoincrement unique not null,
+	width integer not null,
+	height integer not null,
+	digest text not null,
+	data blob not null
+);
+
+create table if not exists thumb_t (
+	imageid integer unique not null 
+		references image_t(id) on update cascade on delete cascade,
+	width integer not null,
+	height integer not null,
+	digest text unique not null,
+	data blob not null
+);
+
+create table if not exists tag_t (
+	id integer primary key autoincrement unique not null,
+	name text unique not null
+);
+
+create table if not exists tag_assign_t (
+	imageid integer not null references image_t(id) on update cascade on delete cascade,
+	tagid integer not null references tag_t(id) on update cascade on delete cascade
+);
